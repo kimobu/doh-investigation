3cd6d2c33f1ef120f8f269753ebc3c8353defb95
Kimo <kimobu@me.com>
2019-12-31 15:34:33 -0500

    Added argument parsing

--- gen_doh.py
+++ gen_doh.py
@@ -1,12 +1,21 @@
+import argparse
 import csv
 import subprocess
 
-with open('majestic_million.csv', newline='') as csvfile:
+parser = argparse.ArgumentParser(description="Generate DNS over HTTPS traffic using doh-client")
+parser.add_argument('csvfile', metavar='csvfile', default="majestic_million.csv", help="CSV file of domains, default to majestic_million.csv")
+parser.add_argument('--post', dest="post", action="store_true", default=False, help="Use HTTP POST instead of GET")
+args = parser.parse_args()
+
+with open(args.csvfile, newline='') as csvfile:
     urlreader = csv.reader(csvfile, delimiter=',')
     count = 0
     for row in urlreader:
         if count == 100000:
             exit()
         domain = row[2]
-        subprocess.Popen(['doh-client', '--cafile', 'selfsigned.pem', '--domain', 'doh-server', '--qname', domain, '--post'])
+        if args.post:
+            subprocess.Popen(['doh-client', '--cafile', 'selfsigned.pem', '--domain', 'doh-server', '--qname', domain, '--post'])
+        else:
+            subprocess.Popen(['doh-client', '--cafile', 'selfsigned.pem', '--domain', 'doh-server', '--qname', domain])
         count += 1
