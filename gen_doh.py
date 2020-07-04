8aaf4ca1950f1e84995ece3a0d802d86007afc7b
Kimo <kimobu@e.com>
2020-07-04 07:03:56 -0600

    Updating for doh2dns

--- gen_doh.py
+++ gen_doh.py
@@ -3,19 +3,20 @@ import csv
 import subprocess
 
 parser = argparse.ArgumentParser(description="Generate DNS over HTTPS traffic using doh-client")
-parser.add_argument('csvfile', metavar='csvfile', default="majestic_million.csv", help="CSV file of domains, default to majestic_million.csv")
-parser.add_argument('--post', dest="post", action="store_true", default=False, help="Use HTTP POST instead of GET")
+parser.add_argument("csvfile", metavar="csvfile", default="majestic_million.csv", help="CSV file of domains, default to majestic_million.csv")
+parser.add_argument("--post", dest="post", action="store_true", default=False, help="Use HTTP POST instead of GET")
+parser.add_argument("--count", dest="count", default="10000", help="Count of DoH packets to generate")
 args = parser.parse_args()
 
 with open(args.csvfile, newline='') as csvfile:
     urlreader = csv.reader(csvfile, delimiter=',')
-    count = 0
+    counter = 0
     for row in urlreader:
-        if count == 100000:
+        if counter == args.count:
             exit()
         domain = row[2]
         if args.post:
             subprocess.Popen(['doh-client', '--cafile', 'selfsigned.pem', '--domain', 'doh-server', '--qname', domain, '--post'])
         else:
             subprocess.Popen(['doh-client', '--cafile', 'selfsigned.pem', '--domain', 'doh-server', '--qname', domain])
-        count += 1
+        counter += 1
